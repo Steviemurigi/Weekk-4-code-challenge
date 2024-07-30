38efe19ba41e5a82e51ec9577cfa623523e9e08e
Steve Murigi <steve.murigi@student.moringaschool.com>
2024-07-29 22:58:01 -0500

    More changes, not yet done!

--- index.js
+++ index.js
@@ -1,24 +1,51 @@
-const init = () => {
-    const inputForm = document.querySelector("form");
-  
-    inputForm.addEventListener("submit", (event) => {
-      event.preventDefault();
-      const input = document.querySelector("input#searchByID");
-  
-      fetch(`http://localhost:3000/movies/${input.value}`)
-        .then((response) => response.json())
-        .then((data) => {
-          const title = document.querySelector("section#movieDetails h4");
-          const description = document.querySelector("section#movieDetails p");
-          const runtime = document.querySelector("section#movieDetails p1");
-          const showtime = document.querySelector("section#movieDetails p2")
-  
-          title.innerText = data.title;
-          description.innerText = data.description;
-          runtime.innerText = data.runtime;
-          showtime.innerText = data.showtime;
-        });
-    });
-  };
-  
-  document.addEventListener("DOMContentLoaded", init);
+const movieList = document.getElementById ("movies");
+const movieDetails = document.getElementsByClassName("movieDeatils");
+const moviePoster = document.getElementById("poster");
+const movieTitle = document.getElementById("title");
+const movieRuntime = document.getElementById("runtime");
+const movieShowtime = document.getElementById("showtime");
+const availableTickets = document.getElementById("tickets")
+
+const moviesBnUrl = http://localhost:3000/movies
+
+//function to fetch first movie
+
+function fetchFirstMovie(){
+  fetch(`${moviesBnUrl}/1`)
+  .then(response => response.json())
+  .then(movie => {
+    if (!movie.soldtickets){
+        movie.soldtickets = 0;
+    }
+    displayMovieDetails(movie);
+  })
+}
+
+//function to fetch all movies
+
+function fetchAllMovies(){
+  fetch(moviesBnUrl)
+  .then(response => response.json())
+  .then(movies => {
+    movieList.textcontent = "";
+    movies.forEach(movie =>{
+      const movieListItem = document.createElement('li');
+      movieListItem.textContent = movie.title;
+      movieListItem.className = "movie-item";
+      movieListItem.addEventListener('click', () => {
+        displayMovieDetails(movie);
+        buyTicketBtn.onclick = () => buyTicketHandler (film)
+      });
+      movieList.appendChild(movieListItem)
+    })
+  })
+}
+
+function displayMovieDetails(movie){
+  moviePoster.src = movie.poster;
+  movieTitle.textContent = movie.title;
+  movieRuntime.textContent = `Runtime: ${movie.runtime} minutes`;
+  movieShowtime.textcontent = `Showtime: ${movie.showtime}`;
+  availableTickets.textcontent = ``
+
+}
